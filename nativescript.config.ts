dcc316d3aff93fb9c455ba7cf95a7a078731faec
Jhamy35 <46756087+Jhamy35@users.noreply.github.com>
2025-05-26 20:31:44 +0200

    Update Android SDK path configuration

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -8,7 +8,7 @@ export default {
     v8Flags: '--expose_gc',
     markingMode: 'none',
     maxLogcatObjectSize: 2048,
-    androidHome: process.env.ANDROID_HOME || '/opt/android-sdk',
+    androidHome: '/opt/android-sdk',
     releaseConfig: {
       keyAlias: 'humojiapp',
       keyPassword: 'YOUR_KEY_PASSWORD',
